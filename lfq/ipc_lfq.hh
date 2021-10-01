82e2878c7b2b885dd09a7d26cd29a592fc02986a
Justin Szaday <jjszaday@gmail.com>
2021-10-01 15:45:07 -0500

    add lfq

---
+++ lfq/ipc_lfq.hh
@@ -0,0 +1,100 @@
+#ifndef __IPC_LFQ_HH__
+#define __IPC_LFQ_HH__
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cstdint>
+
+template <typename T, std::size_t N>
+class ipc_lfq {
+  using value_type = T*;
+
+  const int fd;
+  std::atomic<std::size_t> head;
+  std::atomic<std::size_t> count;
+  std::size_t tail;
+  std::array<std::atomic<value_type>, N> data;
+
+ public:
+  ipc_lfq(const int& _) : fd(_), head(0), count(0), tail(0) {
+    for (auto& datum : data) {
+      datum = nullptr;
+    }
+  }
+
+  bool enqueue(T* value) {
+    auto size = std::atomic_fetch_add_explicit(&(this->count), 1ul,
+                                               std::memory_order_acquire);
+    if (size >= N) {
+      std::atomic_fetch_sub_explicit(&(this->count), 1ul,
+                                     std::memory_order_release);
+      return false;
+    }
+    auto head = std::atomic_fetch_add_explicit(&(this->head), 1ul,
+                                               std::memory_order_acquire) %
+                N;
+    auto ret = std::atomic_exchange_explicit(&(this->data[head]), value,
+                                             std::memory_order_release);
+    assert(ret == nullptr);
+    return true;
+  }
+
+  T* dequeue(void) {
+    auto ret = std::atomic_exchange_explicit(
+        &(this->data[this->tail]), (T*)nullptr, std::memory_order_acquire);
+    if (ret == nullptr) {
+      return nullptr;
+    }
+    if (++(this->tail) >= N) this->tail = 0;
+    auto last = std::atomic_fetch_sub_explicit(&(this->count), 1ul,
+                                               std::memory_order_release);
+    assert(last > 0);
+    return ret;
+  }
+
+  static ipc_lfq<T, N>* open(const char* name) {
+    ipc_lfq<T, N>* res = nullptr;
+    auto fd = shm_open(name, O_CREAT | O_EXCL | O_RDWR, 0666);
+    auto size = sizeof(ipc_lfq<T, N>);
+    auto init = fd >= 0;
+
+    if (init) {
+      auto status = ftruncate(fd, size);
+      assert(status >= 0);
+    } else {
+      fd = shm_open(name, O_RDWR, 0666);
+      assert(fd >= 0);
+    }
+
+    res = (ipc_lfq<T, N>*)mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
+                               0);
+    assert(res != MAP_FAILED);
+
+    if (init) {
+      new (res) ipc_lfq<T, N>(fd);
+    }
+
+    return res;
+  }
+
+  static bool close(const char* name, ipc_lfq<T, N>* self) {
+    auto fd = self->fd;
+    auto status = munmap(self, sizeof(ipc_lfq<T, N>)) >= 0;
+    status = status && (::close(fd) >= 0);
+    shm_unlink(name);
+    return status;
+  }
+};
+
+#endif
---
+++ lfq/tester.cc
@@ -0,0 +1,42 @@
+#include "ipc_lfq.hh"
+
+const char* q0_name_ = "queue_0_";
+const char* q1_name_ = "queue_1_";
+constexpr auto num_msgs_ = 16;
+using queue_type_ = ipc_lfq<std::intptr_t, num_msgs_>;
+
+int main(int argc, char** argv) {
+  assert(argc >= 2);
+  auto procNum = atoi(argv[1]);
+
+  auto* q0 = queue_type_::open(q0_name_);
+  auto* q1 = queue_type_::open(q1_name_);
+
+  auto* mine = (procNum == 0) ? q0 : q1;
+  auto* theirs = (procNum == 0) ? q1 : q0;
+
+  printf("%d> process started!\n", procNum);
+
+  for (auto it = 0; it < num_msgs_; it++) {
+    printf("%d> sending messages for it %d.\n", procNum, it);
+    for (std::intptr_t i = 1; i <= num_msgs_; i++) {
+      while (!theirs->enqueue((std::intptr_t*)i))
+        ;
+    }
+
+    printf("%d> waiting for messages for it %d.\n", procNum, it);
+    for (auto i = 1; i <= num_msgs_; i++) {
+      std::intptr_t* j;
+      while ((j = mine->dequeue()) == nullptr)
+        ;
+      assert(i == (std::intptr_t)j);
+    }
+  }
+
+  queue_type_::close(q0_name_, q0);
+  queue_type_::close(q1_name_, q1);
+
+  printf("%d> all tests passed!\n", procNum);
+
+  return 0;
+}
