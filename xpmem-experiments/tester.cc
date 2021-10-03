b57830ab8b3ade639194ae227b0fea0e2ba30148
Justin Szaday <jjszaday@gmail.com>
2021-10-02 20:46:43 -0500

    Integrate the IPC Queue

--- xpmem-experiments/tester.cc
+++ xpmem-experiments/tester.cc
@@ -12,6 +12,7 @@ extern "C" {
 #include <xpmem.h>
 }
 
+#include "../lfq/ipc_lfq.hh"
 #include "tester.decl.h"
 
 // "borrowed" from VADER
@@ -42,13 +43,36 @@ CksvDeclare(xp_mem_segment_, segment_);
 
 static constexpr auto kNumMsgs = 32;
 
+using queue_type_ = ipc_lfq<sm_, kNumMsgs>;
+CksvDeclare(queue_type_ *, send_queue_);
+CksvDeclare(queue_type_ *, recv_queue_);
+
+// clean up the ipc queues (on shutdown)
+void exit_handler(void) {
+  queue_type_::close("charmxx_send_queue", CksvAccess(send_queue_));
+  queue_type_::close("charmxx_recv_queue", CksvAccess(recv_queue_));
+  CkContinueExit();
+}
+
 void setup_segment(void) {
   CksvInitialize(xp_mem_segment_, segment_);
+  CksvInitialize(queue_type_ *, send_queue_);
+  CksvInitialize(queue_type_ *, recv_queue_);
+
   auto *segment = &(CksvAccess(segment_));
   auto segid =
       xpmem_make(0, XPMEM_MAXADDR_SIZE, XPMEM_PERMIT_MODE, (void *)0666);
   CkEnforceMsg(segid != -1, "could not make xpmem segment");
-  new (segment) xp_mem_segment_{.segid = segid, .node = CkMyNode()};
+  auto mine = CkMyNode();
+  new (segment) xp_mem_segment_{.segid = segid, .node = mine};
+
+  // these could be named something like "charmxx_pe0_queue_"
+  CksvAccess(send_queue_) = queue_type_::open("charmxx_send_queue");
+  CksvAccess(recv_queue_) = queue_type_::open("charmxx_recv_queue");
+
+  if (mine == 0) {
+    registerExitFn(reinterpret_cast<CkExitFn>(&exit_handler));
+  }
 }
 
 struct common {
@@ -58,7 +82,22 @@ struct common {
 
   ~common() { delete this->data; }
 
-  void recv_msg(sm_ &&msg) {
+  void pseudo_scheduler(void) {
+    auto *queue =
+        (CkMyPe() == 0) ? CksvAccess(recv_queue_) : CksvAccess(send_queue_);
+    bool status;
+
+    do {
+      sm_ msg;
+      // spin until we receive a message
+      while (!queue->dequeue(&msg))
+        ;
+      // once we do, handle it
+      status = this->recv_msg(msg);
+    } while (status);
+  }
+
+  bool recv_msg(const sm_ &msg) {
     // TODO ( this should be called only once per peer and cached! )
     auto apid = xpmem_get(msg.segid, XPMEM_RDWR, XPMEM_PERMIT_MODE, NULL);
     CkEnforceMsg(apid != -1, "could not get apid!");
@@ -75,7 +114,7 @@ struct common {
     CkEnforceMsg(ctx != (void *)-1, "could not retrieve buffer!");
 
     auto buf = (uintptr_t)ctx + (ptrdiff_t)(remote_ptr - (uintptr_t)base);
-    auto &recvd = *((int *)buf);
+    auto recvd = *((int *)buf);
 
     CkPrintf("%d> received message of %lu bytes containing: %d\n", CkMyPe(),
              msg.size, recvd);
@@ -84,23 +123,22 @@ struct common {
       // pass on the (next) message
       *(this->data) = recvd + 1;
       this->send_msg(this->data, sizeof(int));
-    } else {
-      CkExit();
     }
 
     xpmem_detach(ctx);
     xpmem_release(apid);
+
+    // we continue while there are messages to send/receive
+    return (recvd < (kNumMsgs - 2));
   }
 
   void send_msg(void *data, const std::size_t &size) {
+    auto *queue =
+        (CkMyPe() == 0) ? CksvAccess(send_queue_) : CksvAccess(recv_queue_);
     auto *segid = &(CksvAccess(segment_).segid);
-
     sm_ msg{.segid = *segid, .ptr = data, .size = size};
-
-    this->send_action(std::move(msg));
+    queue->enqueue(msg);
   }
-
-  virtual void send_action(sm_ &&) = 0;
 };
 
 struct main : public CBase_main, public common {
@@ -117,19 +155,21 @@ struct main : public CBase_main, public common {
     *(this->data) = 0x0;
 
     this->send_msg(this->data, sizeof(int));
+
+    CkExitAfterQuiescence();
   }
 
   ~main() { delete this->data; }
-
-  virtual void send_action(sm_ &&msg) override { neighborProxy.recv_msg(msg); }
 };
 
 struct neighbor : public CBase_neighbor, public common {
   CProxy_main mainProxy;
 
-  neighbor(const CProxy_main &_) : mainProxy(_) {}
-
-  virtual void send_action(sm_ &&msg) override { mainProxy.recv_msg(msg); }
+  neighbor(const CProxy_main &_) : mainProxy(_) {
+    // kick off the schedulers now that we're ready to go
+    mainProxy.pseudo_scheduler();
+    thisProxy.pseudo_scheduler();
+  }
 };
 
 #include "tester.def.h"
