1e74a3c4c6cced75a1c8bc3503aaf85900dd717a
Justin Szaday <jjszaday@gmail.com>
2022-02-20 15:46:03 -0500

    Round out example.

--- path2.0/examples/relaxation/pgm.cpp
+++ path2.0/examples/relaxation/pgm.cpp
@@ -4,13 +4,20 @@
 class relaxation_task : public hypercomm::task<relaxation_task> {
   bool converged;
   bool hasLeft, hasRight;
+
+  double tolerance;
+
   int nRecvd, nNeighbors, nWorkers;
   int it, maxIters;
 
 public:
-  relaxation_task(hypercomm::task_payload &&payload) : converged(false), nRecvd(0) {
+  relaxation_task(PUP::reconstruct) {}
+
+  relaxation_task(hypercomm::task_payload &&payload)
+      : converged(false), nRecvd(0), tolerance(-1) {
+    auto args =
+        std::forward_as_tuple(this->nWorkers, this->maxIters, this->tolerance);
     PUP::fromMem p(payload.data);
-    auto args = std::forward_as_tuple(this->nWorkers, this->maxIters);
     p | args;
 
     auto idx = this->index();
@@ -18,7 +25,8 @@ public:
     this->hasRight = (idx + 1) < this->nWorkers;
     this->nNeighbors = (int)this->hasLeft + this->hasRight;
 
-    CkPrintf("task %d of %d created, has %d neighbors.\n", idx, this->nWorkers, this->nNeighbors);
+    CkPrintf("task %d of %d created, has %d neighbors.\n", idx, this->nWorkers,
+             this->nNeighbors);
 
     this->converge_loop();
   }
@@ -26,7 +34,7 @@ public:
   void converge_loop(void) {
     if (!converged && (it++ < maxIters)) {
       this->nRecvd = 0;
-      
+
       this->send_ghosts();
 
       if (this->nNeighbors == 0) {
@@ -35,22 +43,26 @@ public:
         this->suspend<&relaxation_task::receive_ghost>();
       }
     } else {
-      this->wrap_up();
+      this->on_converge();
     }
   }
 
   void send_ghosts(void) {
-    if (this->hasLeft) {
+    auto idx = this->index();
 
+    if (this->hasLeft) {
+      // TODO ( pack ghost data )
+      this->send(idx - 1);
     }
 
     if (this->hasRight) {
-
+      // TODO ( pack ghost data )
+      this->send(idx + 1);
     }
   }
 
   void receive_ghost(hypercomm::task_payload &&payload) {
-    // copy ghost
+    // TODO ( unpack then copy ghost data )
 
     if ((++this->nRecvd) >= this->nNeighbors) {
       this->check_and_compute();
@@ -60,13 +72,39 @@ public:
   }
 
   void check_and_compute(void) {
+    double max_error = this->tolerance * 2;
+
     CkPrintf("%d> iteration %d\n", this->index(), this->it);
-    
+
+    this->all_reduce(max_error, CkReduction::max_double);
+    this->suspend<&relaxation_task::receive_max_error>();
+  }
+
+  void receive_max_error(hypercomm::task_payload &&payload) {
+    auto &max_error = *((double *)payload.data);
+    this->converged = (max_error <= this->tolerance);
     this->converge_loop();
   }
 
-  void wrap_up(void) {
-    CkExit();
+  void on_converge(void) {
+    // signal this task can be cleaned up by the RTS
+    this->terminate();
+
+    if (this->index() == 0) {
+      CkExit();
+    }
+  }
+
+  void pup(PUP::er &p) {
+    p | this->converged;
+    p | this->tolerance;
+    p | this->hasLeft;
+    p | this->hasRight;
+    p | this->nRecvd;
+    p | this->nNeighbors;
+    p | this->nWorkers;
+    p | this->it;
+    p | this->maxIters;
   }
 };
 
@@ -78,8 +116,9 @@ public:
     auto factor = (msg->argc >= 2) ? atoi(msg->argv[1]) : 4;
     auto n = factor * CkNumPes();
     auto maxIters = 100;
+    auto tolerance = 0.005;
     group = hypercomm::workgroup_proxy::ckNew(n);
-    hypercomm::launch<relaxation_task>(group, n, maxIters);
+    hypercomm::launch<relaxation_task>(group, n, maxIters, tolerance);
   }
 };
 
--- path2.0/include/hypercomm/workgroup.hpp
+++ path2.0/include/hypercomm/workgroup.hpp
@@ -34,9 +34,17 @@ PUPbytes(hypercomm::task_id);
 
 namespace hypercomm {
 struct task_base_ {
+  bool active_;
   task_kind_t kind_;
   continuation_id_t continuation_;
-  task_base_(task_kind_t kind) : kind_(kind) {}
+
+  task_base_(task_kind_t kind) : kind_(kind), active_(true) {}
+
+  void pup_base_(PUP::er &p) {
+    CkAssert(active_);
+
+    p | this->continuation_;
+  }
 };
 } // namespace hypercomm
 
@@ -50,23 +58,24 @@ struct task_message : public CMessage_task_message {
   task_kind_t kind;
   char *data;
 
-  static void* alloc(int msgnum, size_t sz, int *sizes, int pb, GroupDepNum groupDepNum) {
+  static void *alloc(int msgnum, size_t sz, int *sizes, int pb,
+                     GroupDepNum groupDepNum) {
     auto start = ALIGN_DEFAULT(sz);
     auto total_size = start + sizes[0];
-    auto* msg = CkAllocMsg(msgnum, total_size, pb, groupDepNum);
-    static_cast<task_message*>(msg)->data = (char*)msg + start;
+    auto *msg = CkAllocMsg(msgnum, total_size, pb, groupDepNum);
+    static_cast<task_message *>(msg)->data = (char *)msg + start;
     return msg;
   }
 
-  static void* pack(void* raw) {
-    auto* msg = static_cast<task_message*>(raw);
+  static void *pack(void *raw) {
+    auto *msg = static_cast<task_message *>(raw);
     msg->data = msg->data - (std::uintptr_t)msg;
     return raw;
   }
 
-  static void* unpack(void* raw) {
-    auto* msg = static_cast<task_message*>(raw);
-    msg->data = (char*)msg + (std::uintptr_t)msg->data;
+  static void *unpack(void *raw) {
+    auto *msg = static_cast<task_message *>(raw);
+    msg->data = (char *)msg + (std::uintptr_t)msg->data;
     return raw;
   }
 };
@@ -80,9 +89,8 @@ struct task_payload {
       : src(msg), len(UsrToEnv(msg)->getTotalsize() - sizeof(task_message)),
         data(msg->data) {}
 
-  task_payload(CkReductionMsg *msg)
-      : src(msg), len(msg->getLength() - sizeof(task_id)),
-        data((char *)msg->getData() + sizeof(task_id)) {}
+  task_payload(CkReductionMsg *msg, CkReduction::tupleElement *elt)
+      : src(msg), len(elt->dataSize), data((char *)elt->data) {}
 
   task_payload(PUP::reconstruct) {}
 
@@ -98,7 +106,7 @@ struct task_payload {
     } else {
       void *msg = this->src.get();
       CkPupMessage(p, &msg);
-      CmiAssert(msg == this->src.get());
+      CkAssert(msg == this->src.get());
       p | this->len;
       auto offset = (std::uintptr_t)this->data - (std::uintptr_t)msg;
       p | offset;
@@ -115,8 +123,25 @@ template <typename T> struct task : public task_base_ {
 
   template <continuation_fn_t Fn> void suspend(void);
 
+  inline void terminate(void) { this->active_ = false; }
+
+  template <typename Data>
+  void all_reduce(Data &data, CkReduction::reducerType type);
+
+  template <typename Data>
+  void reduce(Data &data, CkReduction::reducerType type, int index);
+
+  template <typename Data>
+  void reduce(Data &data, CkReduction::reducerType type, const CkCallback &cb);
+
+  template <typename... Args>
+  void send(int index, Args&&... args);
+
+  template <typename... Args>
+  void broadcast(Args&&... args);
+
   inline int index(void) const {
-    return static_cast<ArrayElement1D*>(CkActiveObj())->thisIndex;
+    return static_cast<ArrayElement1D *>(CkActiveObj())->thisIndex;
   }
 };
 
@@ -146,7 +171,15 @@ template <typename T> struct task_creation_helper_ {
 };
 
 template <typename T> struct task_puping_helper_ {
-  static void pup(PUP::er &p, task_base_ *&task) {}
+  static void pup(PUP::er &p, task_base_ *&task) {
+    if (p.isUnpacking()) {
+      task = new T(PUP::reconstruct{});
+    }
+
+    task->pup_base_(p);
+
+    ((T *)task)->pup(p);
+  }
 };
 
 task_records_t &get_task_records_(void) {
@@ -187,6 +220,8 @@ template <continuation_t Fn> continuation_id_t register_continuation_(void) {
 template <typename T> task_kind_t task<T>::kind_ = register_task_<T>();
 
 struct workgroup : public CBase_workgroup {
+  template <typename T> friend struct task;
+
   workgroup(void) : last_task_(0) {}
   workgroup(CkMigrateMessage *) {}
 
@@ -203,6 +238,8 @@ struct workgroup : public CBase_workgroup {
 private:
   template <typename T>
   using task_map = std::unordered_map<task_id, T, task_id_hasher_>;
+
+  task_id active_;
   std::uint32_t last_task_;
   task_map<std::unique_ptr<task_base_>> tasks_;
   task_map<std::deque<task_payload>> buffers_;
@@ -214,6 +251,9 @@ private:
 void workgroup::create(task_message *msg) {
   auto creator = get_task_creator_(msg->kind);
   auto tid = msg->tid;
+  // record the active task
+  this->active_ = tid;
+  // then construct the task
   auto ins = this->tasks_.emplace(tid, creator(task_payload(msg)));
   CkAssertMsg(ins.second, "task creation unsucessful!");
   this->flush_buffers_(tid);
@@ -241,8 +281,14 @@ void workgroup::resume(task_message *msg) {
 }
 
 void workgroup::resume(CkReductionMsg *msg) {
-  task_payload payload(msg);
-  auto &tid = *((task_id *)msg->getData());
+  int nReductions;
+  CkReduction::tupleElement *results;
+  msg->toTuple(&results, &nReductions);
+  CkAssert(nReductions == 2);
+  task_payload payload(msg, &results[1]);
+  auto *set = (CkReduction::setElement *)results[0].data;
+  CkAssert(set->next() == nullptr);
+  auto &tid = *((task_id *)set->data);
   if (!this->resume(tid, payload)) {
     this->buffer_(tid, std::move(payload));
   }
@@ -255,7 +301,14 @@ bool workgroup::resume(task_id tid, task_payload &payload) {
   } else {
     auto &task = search->second;
     auto continuation = get_continuation_(task->continuation_);
+    // set the active task
+    this->active_ = tid;
+    // invoke the continuation
     continuation(task.get(), std::move(payload));
+    // then cleanup if the task shutdown
+    if (!task->active_) {
+      this->tasks_.erase(search);
+    }
     return true;
   }
 }
@@ -276,14 +329,8 @@ void workgroup::pup(PUP::er &p) {
 
 using workgroup_proxy = CProxy_workgroup;
 
-template <typename T, typename... Args>
-task_id launch(const workgroup_proxy &group, Args &&...args) {
-  auto *local = group.ckLocalBranch();
-  auto *elems =
-      local ? &(local->*detail::get(detail::backstage_pass())) : nullptr;
-  CmiAssert(elems && !elems->empty());
-
-  auto tid = ((workgroup *)elems->front())->generate_id();
+template <typename... Args>
+task_message* pack(Args &&... args) {
   auto tuple = std::forward_as_tuple(args...);
 
   PUP::sizer s;
@@ -291,12 +338,26 @@ task_id launch(const workgroup_proxy &group, Args &&...args) {
 
   auto size = (int)s.size();
   auto *msg = new (&size) task_message;
-  msg->tid = tid;
-  msg->kind = task<T>::kind_;
 
   PUP::toMem p(msg->data);
   p | tuple;
-  CmiAssert(size == p.size());
+  CkAssert(size == p.size());
+
+  return msg;
+}
+
+template <typename T, typename... Args>
+task_id launch(const workgroup_proxy &group, Args &&...args) {
+  auto *local = group.ckLocalBranch();
+  auto *elems =
+      local ? &(local->*detail::get(detail::backstage_pass())) : nullptr;
+  CkAssert(elems && !elems->empty());
+
+  auto* msg = pack(std::forward<Args>(args)...);
+  auto tid = ((workgroup *)elems->front())->generate_id();
+
+  msg->tid = tid;
+  msg->kind = task<T>::kind_;
 
   const_cast<workgroup_proxy &>(group).create(msg);
 
@@ -320,6 +381,66 @@ template <typename T>
 template <typename task<T>::continuation_fn_t Fn>
 void task<T>::suspend(void) {
   this->continuation_ = continuation_helper_<T, Fn>::id_;
+}
+
+template <typename T>
+template <typename Data>
+void task<T>::all_reduce(Data &data, CkReduction::reducerType type) {
+  auto *host = static_cast<ArrayElement *>(CkActiveObj());
+  this->reduce(data, type,
+               CkCallback(CkIndex_workgroup::resume((CkReductionMsg *)nullptr),
+                          host->ckGetArrayID()));
+}
+
+template <typename T>
+template <typename Data>
+void task<T>::reduce(Data &data, CkReduction::reducerType type, int index) {
+  auto *host = static_cast<ArrayElement *>(CkActiveObj());
+  this->reduce(data, type,
+               CkCallback(CkIndex_workgroup::resume((CkReductionMsg *)nullptr),
+                          CkArrayIndex1D(index), host->ckGetArrayID()));
+}
+
+template <typename T>
+template <typename Data>
+void task<T>::reduce(Data &data, CkReduction::reducerType type,
+                     const CkCallback &cb) {
+  PUP::sizer s;
+  s | data;
+
+  auto size = s.size();
+  char *buf = (char *)alloca(size);
+  PUP::toMemBuf(data, buf, size);
+
+  auto *host = (workgroup *)CkActiveObj();
+  auto &tid = host->active_;
+
+  CkReduction::tupleElement redn[] = {
+      CkReduction::tupleElement(sizeof(task_id), &tid, CkReduction::set),
+      CkReduction::tupleElement(size, buf, type)};
+
+  auto *msg = CkReductionMsg::buildFromTuple(redn, 2);
+  msg->setCallback(cb);
+
+  host->contribute(msg);
+}
+
+template <typename T>
+template <typename... Args>
+void task<T>::send(int index, Args &&... args) {
+  auto *host = (workgroup*)CkActiveObj();
+  auto *msg = pack(std::forward<Args>(args)...);
+  msg->tid = host->active_;
+  host->thisProxy[index].resume(msg);
+}
+
+template <typename T>
+template <typename... Args>
+void task<T>::broadcast(Args &&... args) {
+  auto *host = (workgroup*)CkActiveObj();
+  auto *msg = pack(std::forward<Args>(args)...);
+  msg->tid = host->active_;
+  host->thisProxy.resume(msg);
 }
 } // namespace hypercomm
 
