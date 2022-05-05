99090015aa88d1315960074c6f4111f2e21628c3
Justin Szaday <jjszaday@gmail.com>
2022-05-04 22:29:09 -0400

    implement ping/pong benchmark

--- mpi-qt/schedule.cpp
+++ mpi-qt/schedule.cpp
@@ -1,144 +1,97 @@
-#include <mpi.h>
+#include <warp.hpp>
 
-#include <iostream>
-#include <map>
-#include <memory>
-#include <optional>
-#include <queue>
-#include <vector>
-
-struct message {
-  std::size_t obj;
-  std::size_t ep;
-
-  void* data(void) const { return ((std::byte*)this) + sizeof(message); }
-
-  static std::unique_ptr<message> allocate(std::size_t size) {
-    return std::unique_ptr<message>(
-        (message*)(::operator new(sizeof(message) + size)));
-  }
-};
-
-struct task {
-  virtual ~task() = default;
-};
-
-struct entry_info {
-  using entry_fn_t = void (*)(const std::unique_ptr<task>&,
-                              std::unique_ptr<message>&&);
-
-  entry_fn_t fn;
-  std::optional<std::size_t> size;
-
-  entry_info(entry_fn_t fn_, std::optional<std::size_t>&& size_)
-      : fn(fn_), size(size_) {}
-};
-
-int rank, nRanks;
-std::vector<entry_info> entry_table;
-std::vector<std::unique_ptr<task>> tasks;
-std::map<MPI_Request, std::unique_ptr<message>> sends;
-std::map<MPI_Request, std::unique_ptr<message>> receives;
-std::queue<std::pair<std::size_t, std::size_t>> probes;
-
-int make_tag(std::size_t obj, std::size_t ep) { return (int)(obj + ep); }
-
-void __request(int tag, std::size_t obj, std::size_t ep, std::size_t size) {
-  auto msg = message::allocate(size);
-  MPI_Request req;
-  MPI_Irecv(msg.get(), sizeof(message) + size, MPI_CHAR, MPI_ANY_SOURCE, tag,
-            MPI_COMM_WORLD, &req);
-  receives.emplace(req, std::move(msg));
-}
-
-void request(std::size_t obj, std::size_t ep) {
-  auto& entry = entry_table[ep];
-  auto& size = entry.size;
-  if (size.has_value()) {
-    auto tag = make_tag(obj, ep);
-    __request(tag, obj, ep, *size);
-  } else {
-    probes.emplace(obj, ep);
-  }
-}
-
-void send(int rank, std::size_t obj, std::size_t ep, std::size_t size,
-          std::unique_ptr<message>&& msg) {
-  msg->obj = obj;
-  msg->ep = ep;
-  MPI_Request req;
-  MPI_Isend(msg.get(), sizeof(message) + size, MPI_CHAR, rank,
-            make_tag(obj, ep), MPI_COMM_WORLD, &req);
-  sends.emplace(req, std::move(msg));
-}
+using namespace warp;
 
 bool running = true;
+int receive_index, nIters = 128;
 
-void ping(const std::unique_ptr<task>&, std::unique_ptr<message>&&) {
-  std::cout << rank << "> received ping" << std::endl;
-  running = false;
-}
+struct pingpong : public task {
+  int index_;
+  int it_;
+  bool warmup_;
+  double start_time_;
+
+  pingpong(int index) : index_(index), it_(0), warmup_(true) {
+    request(index, receive_index);
+  }
+
+  static void receive(std::unique_ptr<task>& obj,
+                      std::unique_ptr<message>&& msg) {
+    auto& self = static_cast<pingpong&>(*obj);
+    auto peer = (self.index_ + 1) % 2;
+
+    auto done = (++self.it_) >= nIters;
+    if (done) {
+      done = self.finish(MPI_Wtime() - self.start_time_);
+    }
+
+    send(peer % nRanks, peer, receive_index, std::move(msg));
+
+    if (!done) {
+      request(self.index_, receive_index);
+    }
+  }
+
+  bool finish(double time) {
+    this->it_ = 0;
+    this->start_time_ = MPI_Wtime();
+
+    if (!this->warmup_) {
+      if (this->index_ == 0) {
+        std::cout << rank << "> roundtrip time was "
+                  << ((1e6 * time) / (double)nIters) << " us" << std::endl;
+      }
+
+      if (this->index_ == rank) {
+        running = false;
+      }
+
+      return true;
+    } else {
+      return (this->warmup_ = false);
+    }
+  }
+};
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nRanks);
 
-  entry_table.emplace_back(&ping, std::nullopt);
+  // signal MPI that we don't care about the order of messages
+  MPI_Info info;
+  MPI_Info_create(&info);
+  MPI_Info_set(info, "mpi_assert_allow_overtaking", "true");
+  MPI_Comm_set_info(MPI_COMM_WORLD, info);
+  MPI_Info_free(&info);
 
-  int lastRank = nRanks - 1;
-  if ((rank == 0) || (rank == lastRank)) {
-    auto msg_size = 1024;
+  int msg_size;
+  nIters = (argc >= 2) ? atoi(argv[1]) : 128;
+  msg_size = (argc >= 3) ? atoi(argv[2]) : 1024;
 
-    send((rank == 0) * lastRank, 0, 0, msg_size, message::allocate(msg_size));
+  entry_table.emplace_back(&pingpong::receive, msg_size);
+  receive_index = entry_table.size() - 1;
 
-    request(0, 0);
+  if (rank == 0) {
+    tasks.emplace_back(new pingpong(rank));
+    if (nRanks == 1) {
+      tasks.emplace_back(new pingpong(1));
+    }
+  } else if (rank == 1) {
+    tasks.emplace_back(nullptr);  // dummy to move over one
+    tasks.emplace_back(new pingpong(rank));
   } else {
     running = false;
   }
 
+  if (rank == 0) {
+    std::cout << rank << "> pingpong with " << nIters << " iterations and "
+              << msg_size << " B payload" << std::endl;
+    send((rank + 1) % nRanks, 1, receive_index, message::allocate(msg_size));
+  }
+
   while (running) {
-    for (auto it = receives.begin(); it != receives.end();) {
-      int flag;
-      auto& [req, msg] = *(it);
-      MPI_Test(const_cast<MPI_Request*>(&req), &flag, MPI_STATUS_IGNORE);
-      if (flag) {
-        auto& entry = entry_table[msg->ep];
-        (entry.fn)(tasks[msg->obj], std::move(msg));
-        it = receives.erase(it);
-      } else {
-        it++;
-      }
-    }
-
-    for (auto it = sends.begin(); it != sends.end();) {
-      int flag;
-      auto& [req, msg] = *(it);
-      MPI_Test(const_cast<MPI_Request*>(&req), &flag, MPI_STATUS_IGNORE);
-      if (flag) {
-        it = sends.erase(it);
-      } else {
-        it++;
-      }
-    }
-
-    while (!probes.empty()) {
-      auto& [obj, ep] = probes.front();
-      auto tag = make_tag(obj, ep);
-      int flag;
-
-      MPI_Status status;
-      MPI_Iprobe(MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &flag, &status);
-
-      if (flag) {
-        int count;
-        MPI_Get_count(&status, MPI_CHAR, &count);
-        __request(tag, obj, ep, (std::size_t)count);
-        probes.pop();
-      } else {
-        break;
-      }
-    }
+    __run_once();
   }
 
   return MPI_Finalize();
---
+++ mpi-qt/warp.hpp
@@ -0,0 +1,135 @@
+#ifndef WARP_HPP
+#define WARP_HPP
+
+#include <mpi.h>
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <vector>
+
+namespace warp {
+
+struct message {
+  std::size_t obj;
+  std::size_t ep;
+  std::size_t size;
+
+  inline void* data(void) const;
+
+  static std::unique_ptr<message> allocate(std::size_t size) {
+    auto* msg = (message*)(::operator new(sizeof(message) + size));
+    msg->size = size;
+    return std::unique_ptr<message>(msg);
+  }
+
+  std::size_t total_size(void) const { return this->size + sizeof(message); }
+};
+
+inline void* message::data(void) const {
+  return ((std::byte*)this) + sizeof(message);
+}
+
+struct task {
+  virtual ~task() = default;
+};
+
+struct entry_info {
+  using entry_fn_t = void (*)(std::unique_ptr<task>&,
+                              std::unique_ptr<message>&&);
+
+  entry_fn_t fn;
+  std::optional<std::size_t> size;
+
+  entry_info(entry_fn_t fn_, std::optional<std::size_t>&& size_)
+      : fn(fn_), size(size_) {}
+};
+
+int rank, nRanks;
+std::vector<entry_info> entry_table;
+std::vector<std::unique_ptr<task>> tasks;
+
+using request_map_t = std::map<MPI_Request, std::unique_ptr<message>>;
+
+request_map_t receives, sends;
+std::queue<std::pair<std::size_t, std::size_t>> probes;
+
+int make_tag(std::size_t obj, std::size_t ep) { return (int)(obj + ep); }
+
+template <typename Fn>
+void __foreach_request(request_map_t& map, const Fn& fn) {
+  for (auto it = map.begin(); it != map.end();) {
+    int flag;
+    auto& [req, msg] = *(it);
+    MPI_Test(const_cast<MPI_Request*>(&req), &flag, MPI_STATUS_IGNORE);
+    if (flag) {
+      fn(std::move(msg));
+      it = map.erase(it);
+    } else {
+      it++;
+    }
+  }
+}
+
+void __request(int tag, std::size_t obj, std::size_t ep, std::size_t size) {
+  auto msg = message::allocate(size);
+  MPI_Request req;
+  MPI_Irecv(msg.get(), msg->total_size(), MPI_CHAR, MPI_ANY_SOURCE, tag,
+            MPI_COMM_WORLD, &req);
+  receives.emplace(req, std::move(msg));
+}
+
+void request(std::size_t obj, std::size_t ep) {
+  auto& entry = entry_table[ep];
+  auto& size = entry.size;
+  if (size.has_value()) {
+    auto tag = make_tag(obj, ep);
+    __request(tag, obj, ep, *size);
+  } else {
+    probes.emplace(obj, ep);
+  }
+}
+
+void send(int rank, std::size_t obj, std::size_t ep,
+          std::unique_ptr<message>&& msg) {
+  msg->obj = obj;
+  msg->ep = ep;
+  MPI_Request req;
+  MPI_Isend(msg.get(), msg->total_size(), MPI_CHAR, rank, make_tag(obj, ep),
+            MPI_COMM_WORLD, &req);
+  sends.emplace(req, std::move(msg));
+}
+
+inline void __run_once(void) {
+  // iterate through all sends (free'ing done ones)
+  __foreach_request(sends, [](auto&&) {});
+  // iterate through all receives (triggering their EP)
+  __foreach_request(receives, [](auto&& msg) {
+    auto& entry = entry_table[msg->ep];
+    (entry.fn)(tasks[msg->obj], std::move(msg));
+  });
+  // iterate through the probe queue...
+  while (!probes.empty()) {
+    auto& [obj, ep] = probes.front();
+    auto tag = make_tag(obj, ep);
+    int flag;
+    // check if the probe is ready
+    MPI_Status status;
+    MPI_Iprobe(MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &flag, &status);
+    // if so...
+    if (flag) {
+      // make the request
+      int count;
+      MPI_Get_count(&status, MPI_CHAR, &count);
+      __request(tag, obj, ep, (std::size_t)count);
+      probes.pop();
+    } else {
+      break;
+    }
+  }
+}
+}  // namespace warp
+
+#endif
